3e6091daaf963c319179770b04a8bb560d6f67b6
shekofaamiri <shekofaamiri1@gmail.com>
2025-07-30 09:11:05 +0330

    elif conditoin code

--- triangle_checker.py
+++ triangle_checker.py
@@ -5,3 +5,5 @@ c = float(input("Enter the third side: "))
       
 if a <= 0 or b <= 0 or c <= 0:
     print("Sides must be positive numbers.")
+elif a + b > c and a + c > b and b + c > a:
+    print("Yes, it is a triangle.")
